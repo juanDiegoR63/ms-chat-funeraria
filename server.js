7e97606eb7579ef337b0a81444803ab76bb4c3fa
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-04-20 23:01:18 -0500

    prueba de conexión inicial con ms-logica-negocio

--- server.js
+++ server.js
@@ -1,182 +1,199 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const cors = require('cors');
+const cors = require("cors");
 
 // instanciar express
 const app = express();
+app.use(express.json()); // Asegúrate de que Express pueda manejar el cuerpo JSON
 
 // crear el servidor web
 const server = http.createServer(app);
 
 // configuración del servidor con las cors
 const io = socketIo(server, {
-    cors: {
-        origin: "http://127.0.0.1:5500",
-        credentials: true
-    }
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  },
 });
-
 
 let users = {};
 
 app.get("/", (req, res) => {
-    res.send("Server chat is running");
+  res.send("Server chat is running");
+});
+
+app.post("/chat", (req, res) => {
+  const codigo = req.body.codigo;
+  console.log(`Código recibido: ${codigo}`);
+  res.send("Código recibido");
 });
 
 io.on("connection", (socket) => {
-    console.log("An user connected");
+  console.log("An user connected");
 
-    socket.on("join", (username) => {
-        
-        console.log(`${username} joined the chat with socketId ${socket.id}`)
-        users[socket.id] = username;
+  socket.on("crearSala", (codigoUnico) => {
+    socket.join(codigoUnico);
+    console.log(`Sala de chat ${codigoUnico} creada`);
+  });
+  socket.on("join", (username) => {
+    if (!username) {
+      console.log("Username is undefined");
+      return;
+    }
 
-        const sql = `SELECT idusuario FROM usuario WHERE usuario = "${username}"`;
-        conexion.query(sql, [username], (error, results, fields) => {
-            if (error) {
-                console.error('Error al ejecutar la consulta:', error);
-                return;
-            }
+    console.log(`${username} joined the chat with socketId ${socket.id}`);
+    users[socket.id] = username;
 
-            // Verificar si se encontró algún resultado
-            if (results.length > 0) {
-                const usuarioid = results[0].usuarioid;
-                console.log(`la id del usuario "${username}" es: ${usuarioid}`);
+    const sql = `SELECT idusuario FROM usuario WHERE usuario = "${username}"`;
+    conexion.query(sql, [username], (error, results, fields) => {
+      if (error) {
+        console.error("Error al ejecutar la consulta:", error);
+        return;
+      }
 
-            } else {
-                GuardarUsuario(username);
-            }
-        });
+      // Verificar si se encontró algún resultado
+      if (results.length > 0) {
+        const usuarioid = results[0].usuarioid;
+        console.log(`la id del usuario "${username}" es: ${usuarioid}`);
+      } else {
+        GuardarUsuario(username);
+      }
     });
+  });
 
-    socket.on("message", (message) => {
+  socket.on("message", (message) => {
+    const user = users[socket.id] || "User";
+    const sql = `SELECT estado FROM usuario WHERE usuario = "${user}"`;
+    conexion.query(sql, [user], (error, results, fields) => {
+      if (error) {
+        console.error("Error al ejecutar la consulta:", error);
+        return;
+      }
 
-        const user = users[socket.id] || "User";
-        const sql = `SELECT estado FROM usuario WHERE usuario = "${user}"`;
-        conexion.query(sql, [user], (error, results, fields) => {
-            if (error) {
-                console.error('Error al ejecutar la consulta:', error);
-                return;
-            }
+      // Verificar si se encontró algún resultado
+      if (results.length > 0) {
+        const estado = results[0].estado;
 
-            // Verificar si se encontró algún resultado
-            if (results.length > 0) {
-                const estado = results[0].estado;
-                
-                console.log(`El estado del usuario "${user}" es: ${estado}`);
-                if (estado == true) {
-                    io.emit("message", { user, message, date: new Date() });
-                    GuardarMensaje(user, message, new Date(), 1);
-                    return;
-                }
-            }
-        });
-            
-    });
-
-    socket.on("privateMessage", (data) => {
-        const user = users[socket.id] || "User";
-        const recipientSocket = Object.keys(users).find(
-            (socketId) => users[socketId] === data.recipient
-        );
-        if (recipientSocket) {
-            io.to(recipientSocket).emit("privateMessage", {
-                user,
-                recipient: data.recipient,
-                message: data.message,
-            });
+        console.log(`El estado del usuario "${user}" es: ${estado}`);
+        if (estado == true) {
+          io.emit("message", { user, message, date: new Date() });
+          GuardarMensaje(user, message, new Date(), 1);
+          return;
         }
+      }
     });
+  });
 
-    socket.on("disconnect", () => {
-        console.log(`The user ${users[socket.id]} has left the chat.`)
-        delete users[socket.id];
-    });
+  socket.on("privateMessage", (data) => {
+    const user = users[socket.id] || "User";
+    const recipientSocket = Object.keys(users).find(
+      (socketId) => users[socketId] === data.recipient
+    );
+    if (recipientSocket) {
+      io.to(recipientSocket).emit("privateMessage", {
+        user,
+        recipient: data.recipient,
+        message: data.message,
+      });
+    }
+  });
 
-    // crear el socket.on para cargar mensajes anteriores
-    socket.on("loadMessages", (username) => {
-        CargarMensajes(1);
-    });
+  socket.on("disconnect", () => {
+    console.log(`The user ${users[socket.id]} has left the chat.`);
+    delete users[socket.id];
+  });
+
+  // crear el socket.on para cargar mensajes anteriores
+  socket.on("loadMessages", (username) => {
+    CargarMensajes(1);
+  });
 });
 
 const PORT = process.env.PORT || 3010;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
-let mysql = require('mysql');
-
+let mysql = require("mysql");
 
 let conexion = mysql.createConnection({
-    host: "mysqlaws.cfauaecs6vva.us-east-1.rds.amazonaws.com",
-    database: "chat",
-    user: "admin",
-    password: "N0qC697G6C8O",
-    port: 3306
+  host: "mysqlaws.cfauaecs6vva.us-east-1.rds.amazonaws.com",
+  database: "chat",
+  user: "admin",
+  password: "N0qC697G6C8O",
+  port: 3306,
 });
 
 conexion.connect(function (err) {
-    if (err) {
-        throw (err);
-    } else {
-        console.log("Conexión exitosa a la base de datos");
-    }
+  if (err) {
+    throw err;
+  } else {
+    console.log("Conexión exitosa a la base de datos");
+  }
 });
 
 // crear método para guardar los mensajes en la base de datos
 function GuardarMensaje(username, message, date, idsalachat) {
-    const sql1 = `SELECT idusuario FROM usuario WHERE usuario = "${username}"`;
-    conexion.query(sql1, [username], (error, results, fields) => {
-        if (error) {
-            console.error('Error al ejecutar la consulta:', error);
-            return;
-        }
+  const sql1 = `SELECT idusuario FROM usuario WHERE usuario = "${username}"`;
+  conexion.query(sql1, [username], (error, results, fields) => {
+    if (error) {
+      console.error("Error al ejecutar la consulta:", error);
+      return;
+    }
 
-        // Verificar si se encontró algún resultado
-        if (results.length > 0) {
-            const sql = "INSERT INTO mensajes (usuario, mensaje, fechaHora, idusuario, idsalachat) VALUES (?, ?, ?, ?, ?)";
-            const userId = results[0].idusuario;
-            const values = [username, message, date, userId, idsalachat];
-            console.log(`La ID del usuario "${username}" es: ${userId}`);
-            conexion.query(sql, values, function (err, result) {
-                if (err) {
-                    console.error("Error al insertar el mensaje en la base de datos:", err);
-                } else {
-                    console.log("Mensaje almacenado en la base de datos");
-                }
-            });
+    // Verificar si se encontró algún resultado
+    if (results.length > 0) {
+      const sql =
+        "INSERT INTO mensajes (usuario, mensaje, fechaHora, idusuario, idsalachat) VALUES (?, ?, ?, ?, ?)";
+      const userId = results[0].idusuario;
+      const values = [username, message, date, userId, idsalachat];
+      console.log(`La ID del usuario "${username}" es: ${userId}`);
+      conexion.query(sql, values, function (err, result) {
+        if (err) {
+          console.error(
+            "Error al insertar el mensaje en la base de datos:",
+            err
+          );
         } else {
-            console.log(`No se encontró ningún usuario con el nombre "${username}"`);
+          console.log("Mensaje almacenado en la base de datos");
         }
-    });
-
+      });
+    } else {
+      console.log(`No se encontró ningún usuario con el nombre "${username}"`);
+    }
+  });
 }
 // crear metodo para guardar los usuarios en la base de datos
 function GuardarUsuario(username) {
-    const sql = "INSERT INTO usuario (usuario, estado) VALUES (?, ?)";
-    const values = [username, true];
-    conexion.query(sql, values, function (err, result) {
-        if (err) {
-            console.error("Error al insertar el usuario en la base de datos:", err);
-        } else {
-            console.log("Usuario almacenado en la base de datos");
-        }
-    });
+  const sql = "INSERT INTO usuario (usuario, estado) VALUES (?, ?)";
+  const values = [username, true];
+  conexion.query(sql, values, function (err, result) {
+    if (err) {
+      console.error("Error al insertar el usuario en la base de datos:", err);
+    } else {
+      console.log("Usuario almacenado en la base de datos");
+    }
+  });
 }
 
 // crear una funcion para cargar los mensajes almacenados del chat especifico
 function CargarMensajes(idsalachat) {
-            const sql = `SELECT usuario, mensaje, fechaHora FROM mensajes WHERE idsalachat = ${idsalachat}`;
-            conexion.query(sql, [idsalachat], function (err, result) {
-                if (err) {
-                    console.error("Error al cargar los mensajes del chat:", err);
-                } else {
-                    // escribir los mensajes en pantalla
-                    for (let i = 0; i < result.length; i++) {
-                        io.emit("message", { user: result[i].usuario, message: result[i].mensaje, date: result[i].fechaHora });
-                    }
-                    console.log("Mensajes cargados exitosamente");
-                }
-            });
+  const sql = `SELECT usuario, mensaje, fechaHora FROM mensajes WHERE idsalachat = ${idsalachat}`;
+  conexion.query(sql, [idsalachat], function (err, result) {
+    if (err) {
+      console.error("Error al cargar los mensajes del chat:", err);
+    } else {
+      // escribir los mensajes en pantalla
+      for (let i = 0; i < result.length; i++) {
+        io.emit("message", {
+          user: result[i].usuario,
+          message: result[i].mensaje,
+          date: result[i].fechaHora,
+        });
+      }
+      console.log("Mensajes cargados exitosamente");
+    }
+  });
 }
